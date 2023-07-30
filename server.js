cb8da10cfb01a069fba39a0d41af2cd79a99159f
thib <thibault.barillon@gmail.com>
2023-07-30 22:42:11 +0100

    with logging (can't skimp on quality)

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const http = require("http");
 const hostname = "127.0.0.1";
 const port = 8080;
 
+var again = "";
+
 const server = http.createServer((req, res) => {
+  console.log(
+    "received request from " +
+      req.socket.remoteAddress +
+      " with " +
+      req.headers["user-agent"]
+  );
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World\n");
+  console.log("we did it " + again + "!");
+  again += "again ";
 });
 
 server.listen(port, () => {
